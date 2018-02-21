b43c770fc76ae07445d586aeea4ca8637fd59e6d
Sumit <sumit.javarnd@gmail.com>
2018-02-22 01:32:20 +0530

    Assignment - WarningAlert and SuccessAlert Components

---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: '[app-servers]',
+  //templateUrl: './servers.component.html',
+  //template: '<app-server></app-server><app-server></app-server><app-server></app-server>',
+  template: `
+            <app-server></app-server>
+            <app-server></app-server>
+            <app-server></app-server>
+            <app-server></app-server>
+            `,
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
